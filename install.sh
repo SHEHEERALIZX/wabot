eb399cbf235b37efd126d1d20f8809132d88adbd
SHEHEERALIZX <nssheheerali@gmail.com>
2021-03-29 20:41:21 +0530

    initail commit

---
+++ install.sh
@@ -0,0 +1,51 @@
+termux-setup-storage
+
+apt update
+
+apt upgrade -y
+
+pkg install git 
+
+git clone https://github.com/phaticusthiccy/WhatsAsenaDuplicated
+
+pkg install python
+
+ pkg install ruby
+
+ pkg install git
+
+pkg install php
+
+pkg install perl 
+
+pkg install bash
+
+ pkg install clang
+
+ pkg install nano
+
+ pkg install curl
+
+ pkg install zip
+
+pkg install unzip
+
+pkg install wget  
+
+pkg install openssl
+
+pkg install openssh 
+
+ pkg install sl
+
+pkg install zsh
+
+pkg install nodejs
+
+pkg install ffmpeg
+
+pkg install w3m
+
+ apt update 
+
+ apt upgrade -y
